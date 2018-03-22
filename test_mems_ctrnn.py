a29606284eee28010bcdc79a7516f3abddd66064
Mostafa <mostafa.rafaie@gmail.com>
2018-03-22 11:27:47 -0500

    Add test checking the load of network file

--- test_mems_ctrnn.py
+++ test_mems_ctrnn.py
@@ -14,12 +14,14 @@ class TestMEMS_CTRNN(unittest.TestCase):
         self.assertEqual(math.floor(inverse_sigmoid(0.5)), 0)
 
     def test_CTRNN_load(self):
-        pass
-        # c = CTRNN()
-        # c.load('categorize.ns')
-        #
-        # self.assertEqual(c.size, 14)
-        # self.assertEqual(int(c.taus[0]), 1)
-        # self.assertEqual(int(c.biases[0]), -3)
-        # self.assertEqual(int(c.gains[0]), 2)
-        # self.assertEqual(c.weights[13][0], 0)
+        c = MEMS_CTRNN()
+        c.load('sample.ns')
+
+        self.assertEqual(c.size, 14)
+        self.assertEqual(c.mem_h, 2.6e-6)
+        self.assertEqual(c.mem_rho, 2330)
+        self.assertEqual(c.ythr, -3e-5)
+        self.assertEqual(int(c.taus[0]), 1)
+        self.assertEqual(int(c.v_biases[0]), -3)
+        self.assertEqual(int(c.hs[0]), 2)
+        self.assertEqual(c.weights[13][0], 0)
