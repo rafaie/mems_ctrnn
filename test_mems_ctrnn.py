1cdfd38ab7cb9f5a348993220353508a82fd305a
Mostafa <mostafa.rafaie@gmail.com>
2018-03-22 10:28:46 -0500

    Add a unit test for mems_ctrnn class

---
+++ test_mems_ctrnn.py
@@ -0,0 +1,25 @@
+import unittest
+import math
+from utils import sigmoid, inverse_sigmoid
+from mems_ctrnn import MEMS_CTRNN
+
+
+class TestMEMS_CTRNN(unittest.TestCase):
+    def test_sigmoid(self):
+        self.assertEqual(math.floor(sigmoid(12)), 0)
+        self.assertEqual(sigmoid(0), 0.5)
+        self.assertEqual(math.ceil(sigmoid(12)), 1)
+
+    def test_inverseSigmoid(self):
+        self.assertEqual(math.floor(inverse_sigmoid(0.5)), 0)
+
+    def test_CTRNN_load(self):
+        pass
+        # c = CTRNN()
+        # c.load('categorize.ns')
+        #
+        # self.assertEqual(c.size, 14)
+        # self.assertEqual(int(c.taus[0]), 1)
+        # self.assertEqual(int(c.biases[0]), -3)
+        # self.assertEqual(int(c.gains[0]), 2)
+        # self.assertEqual(c.weights[13][0], 0)
