f983b95aa2bf97f8837e07ff742eb0084856e107
Mostafa <mostafa.rafaie@gmail.com>
2018-03-26 10:38:43 -0500

    Complete the simple mems 2n ctrnn

--- simple_2n_mems_ctrn.py
+++ simple_2n_mems_ctrn.py
@@ -1,4 +1,13 @@
 from mems_ctrnn import MEMS_CTRNN
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def gen_input(t):
+    i1 = 1 if t > 0.001 else 0
+    i2 = 1 if t > 0.005 else 1
+
+    return (i1, i2)
 
 
 if __name__ == "__main__":
@@ -9,7 +18,26 @@ if __name__ == "__main__":
 
     c.print_model()
     c.print_model_abstract()
-    # run_duration = 250
-    # for time in range(int(run_duration/step_size)):
-    #     c.euler_step(step_size)
-    #     print(round(time*step_size, 2), c.neuron_output(0), c.neuron_output(1))
+    run_duration = 30
+    step_size = 0.0005
+
+    out1 = []
+    out2 = []
+
+    for time in np.arange(0.0, run_duration, step_size):
+        i1, i2 = gen_input(time)
+        c.set_neuron_external_input(0, i1)
+        c.set_neuron_external_input(0, i2)
+        c.euler_step(step_size)
+
+        out1.append((time, c.states[0]))
+        out2.append((time, c.states[1]))
+
+    print(out1, out2)
+    out1_np = np.array(out1)
+    out2_np = np.array(out2)
+
+    plt.plot(out1_np[:, 0], out1_np[:, 1], "b-", label='Output 1')
+    plt.plot(out2_np[:, 0], out2_np[:, 1], "r-", label='Output 2')
+    plt.legend()
+    plt.show()
